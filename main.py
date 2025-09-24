ff59227a957d49e3e03fe63c3f89a9873babe244
Filipp Voronov <fivoronov@gmail.com>
2025-09-24 19:11:05 +0300

    Travel calculator implemented

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 msc_price = 13_000
 spb_price = 10_000
 ekb_price = 8_000
+
+dist = input("Введите пункт поездки (msc, spb, ekb): ")
+count = int(input("Введите количество людей: "))
+
+if dist == "msc":
+    price = msc_price * count
+elif dist == "spb":
+    price = spb_price * count
+else:
+    price = ekb_price * count
+
+print("Цена поездки:", price)
